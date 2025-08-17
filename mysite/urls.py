d4b0bee41870a7d6d059f87c21697019723c75e8
Faeze Hoseini Nejad <rhwxhoseininejad@gmail.com>
2025-08-17 16:01:49 +0330

    Adding a template to the site and adding a blog application

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('website.urls')),
+    path('blog/', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
