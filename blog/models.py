c543301a703da760d74f68514cdb4a040df6251a
Faeze Hoseini Nejad <rhwxhoseininejad@gmail.com>
2025-08-19 17:05:06 +0330

    connected classes to database and managed admin pane;
    connected classes to database and added titles and contents.
    managed admin panel.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
 class post(models.Model):
+    #image
+    #author
     title = models.CharField(max_length=225)
     content = models.TextField()
+    #tags
+    #category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null = True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date =models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.id}'
--- website/models.py
+++ website/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+class Contact(models.Model):
+    name = models.CharField(max_length=225)
+    email =models.EmailField()
+    subject = models.CharField(max_length=225)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_date']
+    def __str__(self):
+        return self.name
