08cb2e91026d3d4fc4fae2dbd7dc5d35adb65e7e
Faeze Hoseini Nejad <rhwxhoseininejad@gmail.com>
2025-09-03 17:24:17 +0330

    adding captcha to admin panel

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,6 +57,10 @@ SITE_ID = 2
 # robots
 ROBOTS_US_HOST = False
 ROBOTS_US_SITEMAP = False
+
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'simple-captcha',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
